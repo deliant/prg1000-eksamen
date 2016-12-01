dd03a9d216f8b9c4572baf2d60b3d6c028c1e8b7
Marius Norheim <marius@valknuta.no>
2016-12-01 18:37:13 +0100

    Lagt til PHP funksjon for "Vis flygning"

---
+++ libs/vis-flygning.php
@@ -0,0 +1,19 @@
+<?php
+function visFlygning() {
+  // Åpne filen flygning.txt
+  $fil = fopen("data/flygning.txt", "r");
+  // Skriv data i flygning.txt til en array og lag HTML table
+  print("<table width='100%'>");
+  print("<tr><td><strong>Flightnr</strong></td><td><strong>Fra flyplass</strong></td><td><strong>Til flyplass</strong></td><td><strong>Dato</strong></td></tr>");
+  while($tekstlinje = fgets($fil)) {
+    if($tekstlinje != "") {
+      $tekst = explode(';', $tekstlinje);
+      $tekst = array_map('trim', $tekst);
+      print("<tr><td>$tekst[0]</td><td>$tekst[1]</td><<td>$tekst[2]</td><<td>$tekst[3]</td></tr>");
+    }
+  }
+  print("</table>");
+  // Lukk filen flygning.txt
+  fclose($fil);
+}
+?>
