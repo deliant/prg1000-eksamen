aa4b5f1bd599661453e74a01e3472597a26385c9
Marius Norheim <marius@valknuta.no>
2016-12-01 14:32:08 +0100

    Lagt til index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Eksamen - PRG1000 HBV">
+  <meta name="author" content="Ølgruppa">
+
+  <title>Bjarum Airlines - Ølgruppa</title>
+
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/bootstrap-theme.css" rel="stylesheet">
+</head>
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+
+</body>
+</html>
