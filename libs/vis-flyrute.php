c3a756a91bb8b5d95b9857b9b392b6bdf42ef5ac
Marius Norheim <marius@valknuta.no>
2016-12-01 18:36:47 +0100

    Lagt til PHP funksjon for "Vis flyrute"

---
+++ libs/vis-flyrute.php
@@ -0,0 +1,19 @@
+<?php
+function visFlyrute() {
+  // Åpne filen flyrute.txt
+  $fil = fopen("data/flyrute.txt", "r");
+  // Skriv data i flyrute.txt til en array og lag HTML table
+  print("<table width='100%'>");
+  print("<tr><td><strong>Fra flyplass</strong></td><td><strong>Til flyplass</strong></td></tr>");
+  while($tekstlinje = fgets($fil)) {
+    if($tekstlinje != "") {
+      $tekst = explode(';', $tekstlinje);
+      $tekst = array_map('trim', $tekst);
+      print("<tr><td>$tekst[0]</td><td>$tekst[1]</td></tr>");
+    }
+  }
+  print("</table>");
+  // Lukk filen flyrute.txt
+  fclose($fil);
+}
+?>
