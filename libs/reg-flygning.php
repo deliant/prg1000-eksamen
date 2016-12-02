5939f44bcc7ea87bdac6f79ababeba73ff1c474e
Marius Norheim <marius@valknuta.no>
2016-12-02 16:51:26 +0100

    Fiksa print statements

--- libs/reg-flygning.php
+++ libs/reg-flygning.php
@@ -6,7 +6,7 @@ function regFlygning($flightnr, $fraflyplass, $tilflyplass, $dato) {
     $fil = fopen("data/flygning.txt", "a");
     // Skriv til filen flygning.txt
     fwrite($fil, "$flightnr;$fraflyplass;$tilflyplass;$dato\n");
-    print("<div class='alert alert-success' role='alert'>" Flygningen  . $fraflyplass . " - "  . $tilflyplass . " (" . $flightnr . ") som utføres " . $dato . " registrert i flygningsdatabasen.</div>");
+    print("<div class='alert alert-success' role='alert'>Flygningen " . $fraflyplass . " - "  . $tilflyplass . " (" . $flightnr . ") som utføres " . $dato . " registrert i flygningsdatabasen.</div>");
     // Lukk filen flygning.txt
     fclose($fil);
   }
--- libs/reg-flyrute.php
+++ libs/reg-flyrute.php
@@ -6,7 +6,7 @@ function regFlyrute($fraflyplass, $tilflyplass) {
     $fil = fopen("data/flyrute.txt", "a");
     // Skriv til filen flyrute.txt
     fwrite($fil, "$fraflyplass;$tilflyplass\n");
-    print("<div class='alert alert-success' role='alert'>" Ruten  . $fraflyplass . " - "  . $tilflyplass . " registrert i flyrutedatabasen.</div>");
+    print("<div class='alert alert-success' role='alert'>Ruten " . $fraflyplass . " - "  . $tilflyplass . " registrert i flyrutedatabasen.</div>");
     // Lukk filen flyrute.txt
     fclose($fil);
   }
