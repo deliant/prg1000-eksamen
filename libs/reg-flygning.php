6402677f29cfcd722ebc050d04e73844ecebd18d
Marius Norheim <marius@valknuta.no>
2016-12-02 14:24:39 +0100

    Lagt til PHP funksjon for "Registrer Flygning"

--- libs/reg-flygning.php
+++ libs/reg-flygning.php
@@ -10,11 +10,11 @@ function regFlygning($flightnr, $fraflyplass, $tilflyplass, $dato) {
     $fil = fopen("data/flygning.txt", "a");
     // Skriv til filen flygning.txt
     fwrite($fil, "$flightnr;$fraflyplass;$tilflyplass;$dato\n");
-    print("<div id='melding'>" Flygningen  . $fraflyplass . " - "  . $tilflyplass . " (" . $flightnr . ") som utføres " . $dato . " registrert i flygningsdatabasen.</div>");
+    print("<span class='label label-success'>" Flygningen  . $fraflyplass . " - "  . $tilflyplass . " (" . $flightnr . ") som utføres " . $dato . " registrert i flygningsdatabasen.</span>");
     // Lukk filen flygning.txt
     fclose($fil);
   }
   else {
-    print("<div id='melding'>Mangler gyldig tekstfelt, vennligst fyll inn.</div>");
+    print("<span class='label label-danger'>Mangler gyldig tekstfelt, vennligst fyll inn.</span>");
   }
 }
