96819f1bd9dee1e9bd182a5926f48eaf3c7de28c
Marius Norheim <marius@valknuta.no>
2016-12-03 01:11:44 +0100

    Fiksa funksjon for "Vis Ankomst"

--- libs/vis-ankomst.php
+++ libs/vis-ankomst.php
@@ -1,21 +1,25 @@
 <?php
 function visAnkomst() {
-  $flyplassnavn = trim($_POST["flyplassnavn"]);
-  // Åpne filen flygning.txt
-  $fil = fopen("data/flygning.txt", "r");
-  // Skriv data i flygning.txt til en array og lag HTML table
-  print("<tr><th>Flightnr</th><th>Fra flyplass</th><th>Til flyplass</th><th>Dato</th></tr>");
-  while($tekstlinje = fgets($fil)) {
-    if($tekstlinje != "") {
-      $tekst = explode(';', $tekstlinje);
-      $tekst = array_map('trim', $tekst);
-      /* Ikke ferdig */
-      if(in_array($flyplassnavn, $tekst)) {
-        print("<tr><td>$tekst[0]</td><td>$tekst[1]</td><td>$tekst[2]</td><td>$tekst[3]</td></tr>");
+  $flyplasskode = trim($_POST["flyplasskode"]);
+  if(!empty($flyplasskode)) {
+    // Åpne filen flygning.txt
+    $fil = fopen("data/flygning.txt", "r");
+    // Skriv data i flygning.txt til en array og lag HTML table
+    print("<tr><th>Flightnr</th><th>Fra flyplass</th><th>Til flyplass</th><th>Dato</th></tr>");
+    while($tekstlinje = fgets($fil)) {
+      if($tekstlinje != "") {
+        $tekst = explode(';', $tekstlinje);
+        $tekst = array_map('trim', $tekst);
+        /* Ikke ferdig */
+        if(in_array($flyplasskode, $tekst)) {
+          if($tekst[2] == $flyplasskode) {
+            print("<tr><td>$tekst[0]</td><td>$tekst[1]</td><td>$tekst[2]</td><td>$tekst[3]</td></tr>");
+          }
+        }
       }
     }
+    // Lukk filen flygning.txt
+    fclose($fil);
   }
-  // Lukk filen flygning.txt
-  fclose($fil);
 }
 ?>
