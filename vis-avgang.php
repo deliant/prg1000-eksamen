58274538b8d3d5b64ce3ec315fe9822ed4e25b81
Marius Norheim <marius@valknuta.no>
2016-12-03 01:12:06 +0100

    Fiksa funksjon for "Vis Avgang"

--- vis-avgang.php
+++ vis-avgang.php
@@ -81,14 +81,14 @@
     <div class="panel-body">
       <!-- Husk onsubmit="return validerRegFlyplass()" -->
       <form method="post" id="visavgang" name="visavgang" action="">
-        <label>Flyplassnavn:</label><input type="text" id="flyplassnavn" name="flyplassnavn" onmouseover="musInn(this)" onmouseout="musUt()" onfocus="fokus(this)" onblur="mistetFokus(this)" required /><br />
+        <label>Flyplasskode:</label><input type="text" id="flyplasskode" name="flyplasskode" onmouseover="musInn(this)" onmouseout="musUt()" onfocus="fokus(this)" onblur="mistetFokus(this)" required /><br />
         <label>&nbsp;</label><input type="submit" value="Søk" id="submit" name="submit"><input type="reset" value="Nullstill" id="nullstill" name="nullstill" onclick="fjernMelding()"><br /><br />
       </form>
       <!-- Table -->
       <table class="table-hover" width="100%">
         <?php
         include("libs/vis-avgang.php");
-        if(isset($_POST['submit'])) {
+        if(isset($_POST["submit"])) {
           visAvgang();
         }
         ?>
