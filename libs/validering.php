38bb0df7f8c81becb6100b4d7fa29da155318bf3
Marius Norheim <marius@valknuta.no>
2016-12-04 11:44:03 +0100

    Lagt til PHP validering

--- libs/validering.php
+++ libs/validering.php
@@ -2,15 +2,31 @@
 function validerFlyplasskode($flyplasskode) {
   $lovligFlyplasskode = true;
 
+  // Sjekk at flyplasskode er unik
+  $fil = fopen("data/flygning.txt", "r");
+  while($tekstlinje = fgets($fil)) {
+    if($tekstlinje != "") {
+      $tekst = explode(';', $tekstlinje);
+      $tekst = array_map('trim', $tekst);
+      if($tekst[0] == $flyplasskode) {
+        $lovligFlyplasskode = false;
+      }
+    }
+  }
+  fclose($fil);
+  // Sjekk at form er fyllt ut
   if(!$flyplasskode) {
     $lovligFlyplasskode = false;
   }
+  // Sjekk at flyplasskode er 3 bokstaver
   else if(strlen($flyplasskode) != 3) {
     $lovligFlyplasskode = false;
   }
+  // Sjekk at flyplasskode ikke inneholder nummer
   else if(is_numeric($flyplasskode)) {
     $lovligFlyplasskode = false;
   }
+  // Sjekk at flyplasskode kun innholder bokstaver a-z
   else {
     $tegn1 = substr($flyplasskode,0,1);
     $tegn2 = substr($flyplasskode,1,1);
