375b2083540ccb901e49e43202e80bdbd5d238ce
Marius Norheim <marius@valknuta.no>
2016-12-07 19:01:02 +0100

    Fiksa pathing til datafiler

--- libs/vis-ankomst.php
+++ libs/vis-ankomst.php
@@ -23,7 +23,7 @@ function selectAnkomst() {
 function visAnkomst() {
   $flyplasskode = trim($_POST["flyplasskode"]);
   // Åpne filen flygning.txt
-  $fil = fopen("D:\\Sites\\home.hbv.no\\phptemp\\web-prb10v11/flygning.txt", "r");
+  $fil = fopen("D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v11/flygning.txt", "r");
   // Skriv data i flygning.txt til en array og lag HTML table
   print("<tr><th>Flightnr</th><th>Fra flyplass</th><th>Til flyplass</th><th>Dato</th></tr>");
   while($tekstlinje = fgets($fil)) {
