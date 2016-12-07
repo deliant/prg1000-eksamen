bb9cac0d7aa1661eee1a40e50fba2eb029c41c3d
Marius Norheim <marius@valknuta.no>
2016-12-08 00:55:19 +0100

    Lagt til ajax for 'Registrer flygning'

--- libs/ajax-flygning.php
+++ libs/ajax-flygning.php
@@ -2,13 +2,13 @@
 $flyrute = trim($_GET["flyrute"]);
 // Åpne filen flygning.txt
 $fil = fopen("D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v11/flygning.txt", "r") or die("<div class='alert alert-danger' role='alert'>Kan ikke åpne filen</div>");
-// Skriv data i flyrute.txt til en array og lag HTML table
+// Skriv data i flygning.txt til en array og lag HTML table
 print("<table width='100%'>");
 print("<tr><th>Flightnr</th><th>Fra flyplass</th><th>Til flyplass</th><th>Dato</th></tr>");
 while ($tekstlinje = fgets($fil)) {
   if ($tekstlinje != "") {
     $tekst = explode(';', $tekstlinje);
-    $rute = explode(';', $flyrute)
+    $rute = explode(';', $flyrute);
     $tekst = array_map('trim', $tekst);
     if($rute[0] == $tekst[1] && $rute[1] == $tekst[2]) {
       foreach ($tekst as $array) {
