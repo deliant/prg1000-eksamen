a5c8cda51af8353ad097f9dc142ba21e4ba919b1
Marius Norheim <marius@valknuta.no>
2016-12-07 16:56:19 +0100

    Bytta flyplasskode fra små til store bokstaver

--- js/elements.js
+++ js/elements.js
@@ -8,19 +8,19 @@ function mistetFokus(element) {
 
 function musInn(element) {
   if (element == document.getElementById("flyplasskode")) {
-    document.getElementById("melding").innerHTML="Flyplasskode til flyplassen. Tre små bokstaver. (må være unik)";
+    document.getElementById("melding").innerHTML="Flyplasskode til flyplassen. Tre store bokstaver. (må være unik)";
   }
   if (element == document.getElementById("flyplassnavn")) {
     document.getElementById("melding").innerHTML="Flyplassnavnet er det fullstendige navnet på flyplassen.";
   }
   if (element == document.getElementById("fraflyplass")) {
-    document.getElementById("melding").innerHTML="Flyplassen ruten reiser fra. Tre små bokstaver. (må være unik)";
+    document.getElementById("melding").innerHTML="Flyplassen ruten reiser fra. Tre store bokstaver. (må være unik)";
   }
   if (element == document.getElementById("tilflyplass")) {
-    document.getElementById("melding").innerHTML="Flyplassen ruten reiser til. Tre små bokstaver. (må være unik)";
+    document.getElementById("melding").innerHTML="Flyplassen ruten reiser til. Tre store bokstaver. (må være unik)";
   }
   if (element == document.getElementById("flightnr")) {
-    document.getElementById("melding").innerHTML="Flightnr på flygningen. To små bokstaver, tre tall. (må være unik)";
+    document.getElementById("melding").innerHTML="Flightnr på flygningen. To store bokstaver, tre tall. (må være unik)";
   }
   if (element == document.getElementById("dato")) {
     document.getElementById("melding").innerHTML="Dato, fylles ut i formatet ÅÅÅÅ-MM-DD.";
@@ -31,6 +31,6 @@ function musUt() {
   document.getElementById("melding").innerHTML="";
 }
 
-function smaaBokstaver(element) {
-  element.value=element.value.toLowerCase();
+function storeBokstaver(element) {
+  element.value=element.value.toUpperCase();
 }
