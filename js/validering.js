d2da6762e070161df09a19c9ea81eb5dd9d4f292
Marius Norheim <marius@valknuta.no>
2016-12-08 04:34:57 +0100

    Lagt til javascript validering for dato og flightnr

--- js/validering.js
+++ js/validering.js
@@ -27,17 +27,17 @@ function validerFlyplasskode() {
   return lovligFlyplasskode;
 }
 
-function validerFlightnr() {
-  var flightnr = document.getElementById("flightnr").value;
+function validerFlightnr(flightnr) {
   var tegn1, tegn2, tegn3, tegn4, tegn5;
   var lovligFlightnr = true;
+  var feilMelding = "";
   if(!flightnr) {
     lovligFlightnr = false;
-    document.getElementById("melding").innerHTML="Flightnr ikke fyllt ut.<br />";
+    feilMelding = feilMelding + "Flightnr ikke fyllt ut.<br />";
   }
   else if(flightnr.length != 5) {
     lovligFlightnr = false;
-    document.getElementById("melding").innerHTML="Flightnr har ikke riktig antall karakterer (5).<br />";
+    feilMelding = feilMelding + "Flightnr har ikke riktig antall karakterer (5).<br />";
   }
   else {
     tegn1 = flightnr.substr(0,1);
@@ -47,21 +47,38 @@ function validerFlightnr() {
     tegn5 = flightnr.substr(4,1);
     if(tegn1 < "A" || tegn1 > "Z" || tegn2 < "A" || tegn2 > "Z" || tegn3 < "0" || tegn3 > "9" || tegn4 < "0" || tegn4 > "9" ||  tegn5 < "0" || tegn5 > "9") {
       lovligFlightnr = false;
-      document.getElementById("melding").innerHTML="Flightnr innholder andre karakterer enn A-Z eller tall (store bokstaver).";
+      feilMelding = feilMelding + "Flightnr innholder andre karakterer enn A-Z eller tall (store bokstaver).<br />";
     }
   }
   return lovligFlightnr;
 }
 
-function validerDato() {
-  var dato = document.getElementById("dato").value;
+function validerDato(dato) {
   var regEx = /^\d{4}-\d{2}-\d{2}$/;
+  var lovligDato = true;
+  var feilMelding = "";
   if(!dato.match(regEx)) {
-    return false;
+    lovligDato = false;
+    feilMelding = feilMelding + "Dato har ikke korrekt format (ÅÅÅÅ-MM-DD).<br />";
   }
   var d;
   if(!((d = new Date(dato))|0)) {
-    return false;
+    lovligDato = false;
+    feilMelding = feilMelding + "Ikke gyldig dato. (kan ikke være bak i tid)";
   }
   return d.toISOString().slice(0,10) == dato;
+}
+
+function validerFlygning() {
+  var flightnr = document.getElementById("flightnr").value;
+  var dato = document.getElementById("dato").value;
+  var lovligFlightnr = validerFlightnr(flightnr);
+  var lovligDato = validerDato(dato);
+  if(lovligFlightnr && lovligDato) {
+    return true;
+  }
+  else {
+    document.getElementById("melding").innerHTML=feilMelding;
+    return false;
+  }
 }
