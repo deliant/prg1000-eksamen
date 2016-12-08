8802b8cbfcb83d7782268fdebf2784f7a815224e
Marius Norheim <marius@valknuta.no>
2016-12-08 03:21:06 +0100

    Script

--- libs/remove_lines.php
+++ libs/remove_lines.php
@@ -1,5 +1,5 @@
 <?php
-$lines = file('D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v11/flyplass.txt');
+$lines = file("D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v11/flyplass.txt");
 array_pop($lines);
 $file = join('',$lines);
 ?>
