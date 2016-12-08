0d6c09f20216e215a94d0968dccc9c2ae07ae103
Marius Norheim <marius@valknuta.no>
2016-12-08 04:09:24 +0100

    Lagt til validering for flightnr og dato

--- libs/remove_lines.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lines = file("D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v11/flyplass.txt");
-array_pop($lines);
-
-$contents = implode(';', $lines);
-$file = fopen("D:\\Sites\\home.hbv.no\\phptemp\\web-prg10v11/flyplass.txt", "a") or die("Kan ikke åpne filen");
-fwrite($file, "$contents");
-fclose($file);
-?>
